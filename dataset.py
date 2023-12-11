427a755c8dea749006736835796c869966bceccd
tanzilhassan <tanzilbinhassan@gmail.com>
2023-12-11 09:19:14 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,56 +8,58 @@ import pickle
 
 from skimage import io
 import matplotlib.pyplot as plt
-import numpy
 import torch
 from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import numpy as np
 
-class CIFAR100Train(Dataset):
-    """cifar100 test dataset, derived from
-    torch.utils.data.DataSet
-    """
+class CustomCIFAR100(Dataset):
+    def __init__(self, root, train=True, transform=None, target_transform=None):
+        self.cifar100 = datasets.CIFAR100(
+            root=root, train=train, transform=transform, target_transform=target_transform, download=True
+        )
+        
+        # Define the list of classes to keep (80 classes out of 100)
+        self.selected_classes = list(range(80))
+        self.class_mapping = {class_idx: idx for idx, class_idx in enumerate(self.selected_classes)}
+        
+        # Filter the data and targets to include only the selected classes
+        filtered_data = []
+        filtered_targets = []
+        
+        for data, target in zip(self.cifar100.data, self.cifar100.targets):
+            if target in self.selected_classes:
+                filtered_data.append(data)
+                filtered_targets.append(self.class_mapping[target])
+        
+        self.data = np.array(filtered_data)
+        self.targets = np.array(filtered_targets)
+        
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
 
-    def __init__(self, path, transform=None):
-        #if transform is given, we transoform data using
-        with open(os.path.join(path, 'train'), 'rb') as cifar100:
-            self.data = pickle.load(cifar100, encoding='bytes')
-        self.transform = transform
+        # Convert to PIL Image and apply transforms if provided
+        img = ToTensor()(img)
+        
+        if self.cifar100.transform is not None:
+            img = self.cifar100.transform(img)
+        
+        if self.cifar100.target_transform is not None:
+            target = self.cifar100.target_transform(target)
+
+        return img, target
 
     def __len__(self):
-        return len(self.data['fine_labels'.encode()])
+        return len(self.data)
 
-    def __getitem__(self, index):
-        label = self.data['fine_labels'.encode()][index]
-        r = self.data['data'.encode()][index, :1024].reshape(32, 32)
-        g = self.data['data'.encode()][index, 1024:2048].reshape(32, 32)
-        b = self.data['data'.encode()][index, 2048:].reshape(32, 32)
-        image = numpy.dstack((r, g, b))
+# Example usage
+if __name__ == '__main__':
+    custom_cifar100_train = CustomCIFAR100(root='./data', train=True, transform=ToTensor())
+    custom_cifar100_test = CustomCIFAR100(root='./data', train=False, transform=ToTensor())
 
-        if self.transform:
-            image = self.transform(image)
-        return label, image
+    print("Number of classes:", len(custom_cifar100_train.selected_classes))
+    print("Number of training samples:", len(custom_cifar100_train))
+    print("Number of test samples:", len(custom_cifar100_test))
 
-class CIFAR100Test(Dataset):
-    """cifar100 test dataset, derived from
-    torch.utils.data.DataSet
-    """
-
-    def __init__(self, path, transform=None):
-        with open(os.path.join(path, 'test'), 'rb') as cifar100:
-            self.data = pickle.load(cifar100, encoding='bytes')
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data['data'.encode()])
-
-    def __getitem__(self, index):
-        label = self.data['fine_labels'.encode()][index]
-        r = self.data['data'.encode()][index, :1024].reshape(32, 32)
-        g = self.data['data'.encode()][index, 1024:2048].reshape(32, 32)
-        b = self.data['data'.encode()][index, 2048:].reshape(32, 32)
-        image = numpy.dstack((r, g, b))
-
-        if self.transform:
-            image = self.transform(image)
-        return label, image
 
